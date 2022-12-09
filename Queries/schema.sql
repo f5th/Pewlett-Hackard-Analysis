1eb1674df6e407beaddd54a7f412d07f759e6126
f5th <felipe5th.jR@gmail.com>
2022-12-09 00:35:00 -0500

    SQL Schema create table queries

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -52,7 +52,6 @@ create table titles (
 	title varchar not null,
 	from_date date not null,
 	to_date date not null,
-	foreign key (emp_no) references employees (emp_no),
-	primary key (emp_no)
+	foreign key (emp_no) references employees (emp_no)
 );
 
